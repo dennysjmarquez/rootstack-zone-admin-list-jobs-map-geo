eb5bbf9b5243234b2c8998ca00bb1cd4a2c7a617
Dennys J Marquez <dennysjmarquez@gmail.com>
2021-11-05 19:15:21 -0400

    upCode

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { logout, SESSION_STATE } from './store/actions/sessionActions';
 
